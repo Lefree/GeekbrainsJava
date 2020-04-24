2e2ac05b8da5110d64b5fd61ac7477ba51c1f528
Lefree <alex.lefree@gmail.com>
2020-04-24 14:43:19 +0300

    Избавился от генерации текста файлов в StringBuilder; Изменил метод проверки прохождения пользовательского текста в текст файла;

--- HomeWork_6/src/ru/geekbrains/java/practice/Main.java
+++ HomeWork_6/src/ru/geekbrains/java/practice/Main.java
@@ -4,67 +4,51 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    private static int fileId;
     private static String absDirPath = "/home/alex/IdeaProjects/GeekbrainsJava/HomeWork_6/output/";
 
     public static void main(String[] args) {
         mergeFiles("file_1.txt", "file_2.txt");
 
-        System.out.println(checkTextInFile(absDirPath + "output.txt", "java"));
+        System.out.println(checkTextInFile(absDirPath + "output.txt",
+                "languages in use according to GitHub"));
         System.out.println(checkTextInDirectory(absDirPath, "java"));
     }
 
     /**
      * Метод записывает текст в файл.
      * @param text текст для записи
-     * @return String -имя нового файла
      */
-    private static String writeToFile(String text) {
+    private static void writeToFile(String text) {
         File outputDirectory = new File("output");
         if (!outputDirectory.exists())
             outputDirectory.mkdir();
-        String fileName = "output_" + ++fileId + ".txt";
+        String fileName = "output" + ".txt";
         File newFile = new File(outputDirectory, fileName);
         try {
-            PrintStream ps = new PrintStream(new FileOutputStream(newFile));
+            PrintStream ps = new PrintStream(new FileOutputStream(newFile, true));
             ps.print(text);
             ps.println();
             ps.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return fileName;
-    }
-
-    /**
-     * Метод вычитывает всю информацию из файла
-     * @param fileName имя файла для чтения
-     * @return String - весь текст файла как единая строка
-     */
-    private static String readAllFromFile(String fileName) {
-        StringBuilder sb = new StringBuilder();
-        try {
-            Scanner scanner = new Scanner(new FileInputStream(fileName));
-            while (scanner.hasNext())
-                sb.append(scanner.nextLine());
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return sb.toString();
     }
 
     /**
      * Метод сливает все данные из переданных файлов в один
      * @param fileNames наименования файлов
-     * @return String - имя нового файла, в который записались все данные
      */
-    private static String mergeFiles(String ...fileNames) {
-        StringBuilder sb = new StringBuilder();
+    private static void mergeFiles(String ...fileNames) {
         for (int i = 0; i < fileNames.length; i++) {
-            sb.append(readAllFromFile(fileNames[i]));
+            try {
+                Scanner scanner = new Scanner(new FileInputStream(fileNames[i]));
+                while (scanner.hasNext())
+                    writeToFile(scanner.nextLine());
+                scanner.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
-        return writeToFile(sb.toString());
     }
 
     /**
@@ -74,9 +58,25 @@ public class Main {
      * @return true/false
      */
     private static boolean checkTextInFile(String fileName, String textToFind) {
-        String fileText = readAllFromFile(fileName);
-        return fileText.toLowerCase().indexOf(textToFind.toLowerCase()) >= 0;
-    }
+        char[] textInChars = textToFind.toCharArray();
+        try {
+            FileInputStream fis = new FileInputStream(fileName);
+            int inputByte;
+            int textPosition = 0;
+            while ((inputByte = fis.read()) != -1) {
+                if ((char) inputByte != textInChars[textPosition]) {
+                    textPosition = 0;
+                    continue;
+                } else textPosition++;
+                if (textPosition == textInChars.length - 1)
+                    return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return false;
+}
 
     /**
      * Проверяет содержит ли хотя бы 1 файл из указанной дириктории фрагмент
@@ -85,16 +85,14 @@ public class Main {
      * @param textToFind фрагмент текста для поиска
      * @return true/false
      */
-    private static boolean checkTextInDirectory(String dirPath, String textToFind) {
-        boolean textInDirectory = false;
+    private static boolean checkTextInDirectory(String dirPath, String textToFind) throws NullPointerException{
         File directory = new File(dirPath);
         File[] dirFilesList = directory.listFiles();
         for (int i = 0; i < dirFilesList.length; i++) {
             if (checkTextInFile(dirPath + dirFilesList[i].getName(), textToFind)) {
-                textInDirectory = true;
-                return textInDirectory;
+                return true;
             }
         }
-        return textInDirectory;
+        return false;
     }
 }
